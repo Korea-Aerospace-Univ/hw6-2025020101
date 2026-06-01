72b12b1e9bb2b621ad5296cda10d3b3ef27f5e38
2025020101 <minals0571@gmail.com>
2026-06-01 14:43:50 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char str[11];
+    char *p, *q;
+
+    scanf("%10s", str);
+
+    char maxChar;
+    int maxCount = 0;
+
+    for (p = str; p < str + 10; p++) {
+        int count = 0;
+
+        for (q = str; q < str + 10; q++) {
+            if (*p == *q)
+                count++;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            maxChar = *p;
+        }
+    }
+
+    printf("%c %d", maxChar, maxCount);
+
+    return 0;
+}
